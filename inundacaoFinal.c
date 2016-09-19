4d2124a88eeb667dbf86800ee8e12b9a58099822
wesleybnd <wesleybnd@gmail.com>
2016-09-19 11:10:50 -0300

    atualizacao problema Inundacao

--- inundacaoFinal.c
+++ inundacaoFinal.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <time.h>
+#include <sys/time.h>
 
+#define MICRO_PER_SECOND 1000000 
 typedef struct matriz{
     int status;
     int modificado;
@@ -62,7 +63,6 @@ int i,j;
 int corMaiorPeso(int vetorCores[], int qtdCores, int corAtual){
     int i, cont=0, aux =0;
     for( i = 0 ; i < qtdCores; i++){
-    	//printf("%d = %d\n",i, vetorCores[i]);
         if(vetorCores[i] > aux){
         	aux = vetorCores[i];
             cont = i;
@@ -172,17 +172,16 @@ void visao(Matriz **matriz, int n){
     int i, j;
     for( i = 0; i < n ; i++){
         for( j = 0; j < n ; j++){
-       //     printf("%d ", matriz[i][j].modificado);
+            printf("%d ", matriz[i][j].modificado);
         }
-        //printf("\n");
+        printf("\n");
     }
-   // printf("\n");
+    printf("\n");
 }
 
 Matriz **coloracao(Matriz **matriz, int n ,int corAtual, int cor){
     int i, j;
-	//printf("mardita cor atual = %d \ncor escolhida: %d\n\n", corAtual, cor);
-    for( i = 0; i < n ; i++){
+	for( i = 0; i < n ; i++){
         for( j = 0; j < n ; j++){
             if( i== 0 && j == i){
                 matriz[i][j].modificado =1;
@@ -298,11 +297,9 @@ Matriz **coloracao(Matriz **matriz, int n ,int corAtual, int cor){
                     }
                 }
             }
-            //if(matriz[i][j].modificado == 1) printf("modificou: j = %d i = %d\n",j,i);
             matriz[i][j].status = 0;
         }
     }
-     //printf("saiu coloracao\n");
     return matriz;
 }
 
@@ -328,47 +325,46 @@ int i, j, cont = matriz[0][0].cor;
 }
 
 Matriz **desinundar(Matriz **matriz, int n, int qtdCores){
+    struct timeval start_time;
+    struct timeval stop_time;
+    float time_diff; 
+    gettimeofday( &start_time, NULL );
     int i=0, j=0,y=0,x=0,cont;
     int corAtual = matriz[0][0].cor;
     int vetorCores[qtdCores];
     int resultado=0;
+    float tmili;
     zerarVetorCores(vetorCores, qtdCores);
-    imprime(matriz , n);
+    printf("%d ", matriz[0][0].cor);
     while(1){
     	
     	y++;
         i++;
-        //printf("\nvalor do i = %d\nvalor do j = %d",i,j);
-	    //printf("\nvalor do x = %d\nvalor do y = %d\n\n",x,y);
         if( y == i && i == n){
             j++;
             x = j;
             y = j;
             i = j;
             if( j == n){
-            	x = y = i = j = 0;
+                x = 0; 
+                y = 1;
+                i = 1;
+                j = 0;
             }
             if(matriz[i][j].cor != corAtual) vetorCores[matriz[i][j].cor]++;
-
             marcar(matriz,i,j,n,vetorCores,corAtual);
             resultado = corMaiorPeso(vetorCores, qtdCores, corAtual);
-            //printf("resultado dentro do final = %d\n",resultado );
             if(resultado == corAtual){
                 vetorCores[resultado] = 0;
                 resultado = corMaiorPeso(vetorCores, qtdCores, corAtual);
             }
             	if(resultadoParcial(vetorCores,qtdCores) == 0){
-            	 //printf("verificacao de saida\n");
-		            if(previsaoTermino(matriz, n)==2){
-		          //      printf("verificacao de saida 2\n");
+    	            if(previsaoTermino(matriz, n)==2){
 		                break;
 		            }
           		 }else{
           		 	matriz = coloracao(matriz, n, corAtual, resultado);
           		 }
-                
-            	continue;
-
         }else{
                 if(i+1 > n){
                     i=0;
@@ -397,70 +393,51 @@ Matriz **desinundar(Matriz **matriz, int n, int qtdCores){
                 coloracao(matriz, n, corAtual, matriz[i][j].cor);
                 vetorCores[matriz[i][j].cor]++;
                 resultado = corMaiorPeso(vetorCores, qtdCores, corAtual);
-               //printf("pontuacao i e j = %d\nPontuacao x e y = %d\n",vetorCores[matriz[i][j].cor],
-               	//													 vetorCores[matriz[i][j].cor]);
-               //printf("resultado = %d\n",resultado);
-
+     
             }else if(matriz[i][j].cor != matriz[x][y].cor){
                if(matriz[i][j].cor != corAtual)vetorCores[matriz[i][j].cor]++;
                if(matriz[x][y].cor != corAtual)vetorCores[matriz[x][y].cor]++;
-               //printf("cor atualissima = %d\n",corAtual );
                marcar(matriz,i,j,n,vetorCores,corAtual);
                marcar(matriz,x,y,n,vetorCores,corAtual);
                resultado = corMaiorPeso(vetorCores, qtdCores, corAtual);
-               /*printf("pontuacao i e j = %d\nPontuacao x e y = %d\n",matriz[i][j].cor,
-               														 matriz[x][y].cor);
-               printf("resultado = %d\n",resultado);*/
                if(resultado != matriz[x][y].cor && resultado != matriz[i][j].cor){
-                    //printf("CAGADA\n");
                     vetorCores[resultado] = 0;
                     resultado = corMaiorPeso(vetorCores, qtdCores, corAtual);
                }
-               if(vetorCores[resultado] != 0) matriz = coloracao(matriz, n, corAtual, resultado);
-               
-               
+               if(vetorCores[resultado] != 0) matriz = coloracao(matriz, n, corAtual, resultado);               
             }
         }
             if(resultadoParcial(vetorCores,qtdCores) == 0){
             	continue;
-            }
-
-    //        printf("COR ATUAL = %d\n",corAtual);	    	
-	  //      imprime(matriz , n);
-	    //    printf("\n\n");
-          //  visao(matriz, n);            
-            corAtual = resultado;
+            }            
             (int *)zerarVetorCores(vetorCores, qtdCores);
-            //printf("--------------------------------------------------------\n");
-           
-         //  getchar();
+
+            if(resultado != corAtual)printf("- %d ",corAtual);    
+            corAtual = resultado;        
      }
-     printf("\n\nNOVA\n");
-	imprime(matriz , n);
+    printf("\n");
+    gettimeofday( &stop_time, NULL );
+    time_diff = (float)(stop_time.tv_sec - start_time.tv_sec);
+    time_diff += (stop_time.tv_usec - start_time.tv_usec)/(float)MICRO_PER_SECOND; 
+    printf("\nfuncao executada em %f ms\n",time_diff);
     return matriz;
 }
 
 int main(){
     int n, qtdCores,op;
-    char *cores[11][10] = {"amarelo","verde","vermelho","azul","branco","rosa","marrom","preto","violeta","cinza","bronze"};
-    qtdCores = 12;
-    n =40;
-
-   // scanf("%d %d",&n, &qtdCores);8
-    //setbuf(stdin,NULL);
-    //imprimirCores(cores, qtdCores);
+    scanf("%d %d",&n, &qtdCores);
+    setbuf(stdin,NULL);
     Matriz **matriz = criarMatriz(n);
-    //printf("\nQual modo deseja:\n1 - aleatorio\n2-Manual\nopcao: ");
-    //scanf("%d",&op);
-     inserirModoRandom(matriz, n, qtdCores);
-     /*
+    printf("\nQual modo deseja:\n1 - aleatorio\n2-Manual\nopcao: ");
+    scanf("%d",&op);
+    inserirModoRandom(matriz, n, qtdCores);
+     
     if(op == 1){
         inserirModoRandom(matriz, n, qtdCores);
     }else{
-        printf("insira as cores correspondentes aos numeros\n");
+        printf("insira as cores correspondentes à coluna e linha:\n");
         inserirMatriz(matriz, n);
-    }
-    */
+    }    
     desinundar(matriz, n, qtdCores);
 return 0;
 }
